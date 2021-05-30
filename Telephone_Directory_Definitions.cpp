042d701c7e492a1f1a0a9300a7d928fca865873f
unknown <krishwar2001@gmail.com>
2021-05-30 10:13:40 +0530

    after formatting

--- Telephone_Directory_Definitions.cpp
+++ Telephone_Directory_Definitions.cpp
@@ -3,93 +3,94 @@
 #include <stack>
 #include <vector>
 #include <string>
+#include <cstdio>
 #include <bits/stdc++.h>
 #include "Telephone_Directory.h"
 using namespace std;
 
-Phone_Directory :: Phone_Directory()
+Phone_Directory ::Phone_Directory()
 {
 	nullRecord = new node;
 	nullRecord->left = nullRecord->right = nullRecord;
 	root = nullRecord;
 }
 
-Phone_Directory :: ~Phone_Directory()
+Phone_Directory ::~Phone_Directory()
 {
 	Make_Empty(root);
 	delete root;
 }
 
-void Phone_Directory :: Insert_Record(string phone_num, string name)
+void Phone_Directory ::Insert_Record(string phone_num, string name)
 {
 	node *newRecord = new node;
 	newRecord->left = newRecord->right = nullRecord;
 	newRecord->phone_num = phone_num;
 	newRecord->name = name;
-	
-	if(root == nullRecord)
+
+	if (root == nullRecord)
 		root = newRecord;
-	
+
 	else
 	{
 		splay(phone_num, root);
-		if(phone_num < root->phone_num)
+		if (phone_num < root->phone_num)
 		{
 			newRecord->left = root->left;
 			newRecord->right = root;
 			root->left = nullRecord;
 			root = newRecord;
 		}
-		
-		else if(phone_num > root->phone_num)
+
+		else if (phone_num > root->phone_num)
 		{
 			newRecord->right = root->right;
 			newRecord->left = root;
 			root->right = nullRecord;
 			root = newRecord;
 		}
-		
+
 		else
 			return;
 	}
-	cout << "\nEntry has been successfully inserted into the directory!!\n" ;
+	cout << "\nEntry has been successfully inserted into the directory!!\n";
 }
 
-void Phone_Directory :: splay(string phone_num, node *&t)
+void Phone_Directory ::splay(string phone_num, node *&t)
 {
 	node header;
 	header.left = header.right = nullRecord;
 	node *leftTreeMax, *rightTreeMin;
 	leftTreeMax = rightTreeMin = &header;
 	nullRecord->phone_num = phone_num;
-	while(1)
+	while (1)
 	{
-		if(phone_num < t->phone_num)
+		if (phone_num < t->phone_num)
 		{
-			if(phone_num < t->left->phone_num)
+			if (phone_num < t->left->phone_num)
 				rotate_with_left_child(t);
-			
-			if(t->left == nullRecord)
+
+			if (t->left == nullRecord)
 				break;
-			
+
 			rightTreeMin->left = t;
 			rightTreeMin = t;
-			t=t->left;
+			t = t->left;
 		}
-		
-		else if(phone_num > t->phone_num)
+
+		else if (phone_num > t->phone_num)
 		{
-			if(phone_num > t->right->phone_num)
+			if (phone_num > t->right->phone_num)
 				rotate_with_right_child(t);
-			
-			if(t->right == nullRecord)
+
+			if (t->right == nullRecord)
 				break;
-			
+
 			leftTreeMax->right = t;
 			leftTreeMax = t;
 			t = t->right;
 		}
-		
+
 		else
 			break;
 	}
@@ -99,7 +100,7 @@ void Phone_Directory :: splay(string phone_num, node *&t)
 	t->right = header.left;
 }
 
-void Phone_Directory :: rotate_with_left_child(node *&k2)
+void Phone_Directory ::rotate_with_left_child(node *&k2)
 {
 	node *k1 = k2->left;
 	k2->left = k1->right;
@@ -107,7 +108,7 @@ void Phone_Directory :: rotate_with_left_child(node *&k2)
 	k2 = k1;
 }
 
-void Phone_Directory :: rotate_with_right_child(node *&k2)
+void Phone_Directory ::rotate_with_right_child(node *&k2)
 {
 	node *k1 = k2->right;
 	k2->right = k1->left;
@@ -115,18 +116,18 @@ void Phone_Directory :: rotate_with_right_child(node *&k2)
 	k2 = k1;
 }
 
-void Phone_Directory :: Delete_Record(string phone_num)
+void Phone_Directory ::Delete_Record(string phone_num)
 {
 	node *newTree = NULL;
-	if(!contains(phone_num))
+	if (!contains(phone_num))
 	{
 		cout << phone_num << " is not present in the directory!!\n\n";
 		return;
 	}
-	
-	if(root->right == nullRecord)
+
+	if (root->right == nullRecord)
 		newTree = root->left;
-	
+
 	else
 	{
 		newTree = root->right;
@@ -137,28 +138,28 @@ void Phone_Directory :: Delete_Record(string phone_num)
 	root = newTree;
 }
 
-bool Phone_Directory :: contains(string phone_num)
+bool Phone_Directory ::contains(string phone_num)
 {
 	splay(phone_num, root);
 	return root->phone_num == phone_num;
 }
 
-bool Phone_Directory :: Is_Empty()
+bool Phone_Directory ::Is_Empty()
 {
 	return root == nullRecord;
 }
 
-void Phone_Directory :: Make_Empty(node *&t)
+void Phone_Directory ::Make_Empty(node *&t)
 {
-	while(t != nullRecord)
+	while (t != nullRecord)
 		Delete_Record(t->phone_num);
 }
 
 int Phone_Directory::count = 0;
 
-void Phone_Directory :: Display_Records(node *t)
+void Phone_Directory ::Display_Records(node *t)
 {
-	if(t == NULL)
+	if (t == NULL)
 	{
 		cout << "\nThe phone directory has zero entries!!\n";
 		return;
@@ -170,19 +171,20 @@ void Phone_Directory :: Display_Records(node *t)
 	cout << "--------------------------------------------\n";
 }
 
-void Phone_Directory :: print(node *&t)//Preorder traversal
+void Phone_Directory ::print(node *&t) //Preorder traversal
 {
-	if(t != t->left)
+	if (t != t->left)
 	{
-		cout << t->name << "\t\t" << t->phone_num << endl;
+		//cout << t->name << "\t\t" << t->phone_num << endl;
+		printf("%-24s %s\n", t->name.c_str(), t->phone_num.c_str());
 		print(t->left);
 		print(t->right);
 	}
 }
 
-void Phone_Directory :: search_key(string phone_num)
+void Phone_Directory ::search_key(string phone_num)
 {
-	if(!contains(phone_num))
+	if (!contains(phone_num))
 	{
 		cout << phone_num << " is not present in the directory!!\n";
 		return;
@@ -193,69 +195,68 @@ void Phone_Directory :: search_key(string phone_num)
 	root->count++;
 }
 
-void Phone_Directory :: Partial_Matches(string phone_num)
+void Phone_Directory ::Partial_Matches(string phone_num)
 {
 	vector<node *> Partial_Matched_phonenum;
 	stack<node *> TEMP_Stk;
-	if(Is_Empty())
+	if (Is_Empty())
 	{
 		cout << "\nThe phone directory is empty !!\n";
 		return;
 	}
 	TEMP_Stk.push(root);
 	int count_keys = 1;
-	while(TEMP_Stk.size() != 0)
+	while (TEMP_Stk.size() != 0)
 	{
 		node *temp = TEMP_Stk.top();
 		TEMP_Stk.pop();
 		int m = temp->phone_num.size(), n = phone_num.size();
 		int LCS[m + 1][n + 1];
 		int result = 0;
-		for(int i = 0; i <= m; i++)
+		for (int i = 0; i <= m; i++)
 		{
-			for(int j = 0; j <= n; j++)
+			for (int j = 0; j <= n; j++)
 			{
-				if(i == 0 || j == 0)
+				if (i == 0 || j == 0)
 				{
 					LCS[i][j] = 0;
 					continue;
 				}
 				char L = temp->phone_num[i - 1], R = phone_num[j - 1];
-				
-				
-				
-				if(L == R)
+
+				if (L == R)
 				{
 					LCS[i][j] = LCS[i - 1][j - 1] + 1;
 					//result = max(result, LCS[i][j]);
 				}
-				
+
 				else
 					LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1]);
 			}
 		}
-		if(LCS[m][n] == n)
+		if (LCS[m][n] == n)
 		{
 			cout << count_keys << "." << temp->phone_num << endl;
 			count_keys++;
 			Partial_Matched_phonenum.push_back(temp);
 		}
-		
-		if(temp->right != nullRecord)
+
+		if (temp->right != nullRecord)
 			TEMP_Stk.push(temp->right);
-		
-		if(temp->left != nullRecord)
+
+		if (temp->left != nullRecord)
 			TEMP_Stk.push(temp->left);
 	}
-	if(Partial_Matched_phonenum.size() == 0)
+	if (Partial_Matched_phonenum.size() == 0)
 	{
 		cout << "\nNo partial matches found !!" << endl;
 		return;
 	}
 	int choice;
-	cout << endl << "Enter your choice : ";
+	cout << endl
+		 << "Enter your choice : ";
 	cin >> choice;
-	if(choice < 1 || choice > Partial_Matched_phonenum.size())
+	if (choice < 1 || choice > Partial_Matched_phonenum.size())
 	{
 		cout << "\nInvalid choice !!\n";
 		return;
@@ -268,69 +269,68 @@ void Phone_Directory :: Partial_Matches(string phone_num)
 	splay(Final->phone_num, root);
 }
 
-void Phone_Directory :: Partial_Matches_Name(string name)
+void Phone_Directory ::Partial_Matches_Name(string name)
 {
 	vector<node *> Partial_Matched_name;
 	stack<node *> TEMP_Stk;
-	if(Is_Empty())
+	if (Is_Empty())
 	{
 		cout << "\nThe phone directory is empty !!\n";
 		return;
 	}
 	TEMP_Stk.push(root);
 	int count_keys = 1;
-	while(TEMP_Stk.size() != 0)
+	while (TEMP_Stk.size() != 0)
 	{
 		node *temp = TEMP_Stk.top();
 		TEMP_Stk.pop();
 		int m = temp->name.size(), n = name.size();
 		int LCS[m + 1][n + 1];
 		int result = 0;
-		for(int i = 0; i <= m; i++)
+		for (int i = 0; i <= m; i++)
 		{
-			for(int j = 0; j <= n; j++)
+			for (int j = 0; j <= n; j++)
 			{
-				if(i == 0 || j == 0)
+				if (i == 0 || j == 0)
 				{
 					LCS[i][j] = 0;
 					continue;
 				}
 				char L = temp->name[i - 1], R = name[j - 1];
-				
-				
-				
-				if(L == R)
+
+				if (L == R)
 				{
 					LCS[i][j] = LCS[i - 1][j - 1] + 1;
 					//result = max(result, LCS[i][j]);
 				}
-				
+
 				else
 					LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1]);
 			}
 		}
-		if(LCS[m][n] == n)
+		if (LCS[m][n] == n)
 		{
 			cout << count_keys << "." << temp->name << endl;
 			count_keys++;
 			Partial_Matched_name.push_back(temp);
 		}
-		
-		if(temp->right != nullRecord)
+
+		if (temp->right != nullRecord)
 			TEMP_Stk.push(temp->right);
-		
-		if(temp->left != nullRecord)
+
+		if (temp->left != nullRecord)
 			TEMP_Stk.push(temp->left);
 	}
-	if(Partial_Matched_name.size() == 0)
+	if (Partial_Matched_name.size() == 0)
 	{
 		cout << "\nNo partial matches found !!" << endl;
 		return;
 	}
 	int choice;
-	cout << endl << "Enter your choice : ";
+	cout << endl
+		 << "Enter your choice : ";
 	cin >> choice;
-	if(choice < 1 || choice > Partial_Matched_name.size())
+	if (choice < 1 || choice > Partial_Matched_name.size())
 	{
 		cout << "\nInvalid choice !!\n";
 		return;
@@ -343,53 +343,54 @@ void Phone_Directory :: Partial_Matches_Name(string name)
 	splay(Final->phone_num, root);
 }
 
-void Phone_Directory :: Frequent_keys()
+void Phone_Directory ::Frequent_keys()
 {
 	vector<node *> Frequents;
 	stack<node *> TEMP_Stk;
-	if(Is_Empty())
+	if (Is_Empty())
 	{
 		cout << "\nThe phone directory has zero records !!\n";
 		return;
 	}
 	TEMP_Stk.push(root);
-	while(TEMP_Stk.size() != 0)
+	while (TEMP_Stk.size() != 0)
 	{
 		node *temp = TEMP_Stk.top();
 		TEMP_Stk.pop();
-		if(temp->count > 0)
+		if (temp->count > 0)
 		{
 			Frequents.push_back(temp);
 			sort(Frequents);
 		}
-		
-		if(temp->right != nullRecord)
+
+		if (temp->right != nullRecord)
 			TEMP_Stk.push(temp->right);
-		
-		if(temp->left != nullRecord)
+
+		if (temp->left != nullRecord)
 			TEMP_Stk.push(temp->left);
 	}
-	
-	if(Frequents.size() == 0)
+
+	if (Frequents.size() == 0)
 	{
 		cout << "\nNo phone number was searched\n";
 		return;
 	}
 	cout << endl;
-	for(int i = 0; i < Frequents.size(); i++)
+	for (int i = 0; i < Frequents.size(); i++)
 	{
 		cout << "Name : " << Frequents[i]->name << endl;
 		cout << "Phone number : " << Frequents[i]->phone_num << endl;
-		cout << "Search frequency : " << Frequents[i]->count << endl << endl;
+		cout << "Search frequency : " << Frequents[i]->count << endl
+			 << endl;
 	}
 }
 
-void Phone_Directory :: sort(vector<node *> &Frequents)
+void Phone_Directory ::sort(vector<node *> &Frequents)
 {
 	int size = Frequents.size();
 	node *last = Frequents[size - 1];
 	int i = size - 2;
-	while(i >= 0 && Frequents[i]->count < last->count)
+	while (i >= 0 && Frequents[i]->count < last->count)
 	{
 		Frequents[i + 1] = Frequents[i];
 		i--;
@@ -397,62 +398,62 @@ void Phone_Directory :: sort(vector<node *> &Frequents)
 	Frequents[i + 1] = last;
 }
 
-bool Phone_Directory :: name_found(node * n, const string & str)
+bool Phone_Directory ::name_found(node *n, const string &str)
 {
-    if(n == nullRecord)
-    return false;
-    
-    string t = n->name;
-    transform(t.begin(),t.end(),t.begin(),::tolower);
-    if(t.compare(str) == 0)
-    return true;
+	if (n == nullRecord)
+		return false;
 
-    return name_found(n->left,str) || name_found(n->right,str);
+	string t = n->name;
+	transform(t.begin(), t.end(), t.begin(), ::tolower);
+	if (t.compare(str) == 0)
+		return true;
+
+	return name_found(n->left, str) || name_found(n->right, str);
 }
 
-bool Phone_Directory :: name_found( string & n)
+bool Phone_Directory ::name_found(string &n)
 {
-    transform(n.begin(),n.end(),n.begin(),::tolower);
-    
-    if(root == nullRecord)
-    return false;
-    
-    /*string t = root->name;
+	transform(n.begin(), n.end(), n.begin(), ::tolower);
+
+	if (root == nullRecord)
+		return false;
+
+	/*string t = root->name;
     transform(t.begin(),t.end(),t.begin(),::tolower);
  
     if(t.compare(n)==0)
     return true;
 */
-    //return name_found(root->left,n) || name_found(root->right,n);
-     return name_found(root,n);
+	//return name_found(root->left,n) || name_found(root->right,n);
+	return name_found(root, n);
 }
 
-void Phone_Directory :: search_by_name(node * n, const string & search_string)
+void Phone_Directory ::search_by_name(node *n, const string &search_string)
 {
-    if(n==nullRecord)
-    return;
-    
-    string s = n->name;
-    transform(s.begin(),s.end(),s.begin(),::tolower);
-    
-    if(s.compare(search_string) == 0)
-    {
-    cout << n->name << "\t\t" << n->phone_num << endl;
-	n->count++;
-    }
+	if (n == nullRecord)
+		return;
 
-    search_by_name(n->left,search_string);
-    search_by_name(n->right,search_string);
+	string s = n->name;
+	transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+	if (s.compare(search_string) == 0)
+	{
+		cout << n->name << "\t\t" << n->phone_num << endl;
+		n->count++;
+	}
+
+	search_by_name(n->left, search_string);
+	search_by_name(n->right, search_string);
 }
 
-void Phone_Directory :: search_by_name(const string & search_name)
+void Phone_Directory ::search_by_name(const string &search_name)
 {
-   /*string t = root->name;
+	/*string t = root->name;
    transform(t.begin(),t.end(),t.begin(),::tolower);
    if(t.compare(search_name) == 0)
    cout << root->name << "\t\t" << root->phone_num << endl;
    */
-   //search_by_name(root->left,search_name);
-   //search_by_name(root->right,search_name); 
-   search_by_name(root,search_name);  
+	//search_by_name(root->left,search_name);
+	//search_by_name(root->right,search_name);
+	search_by_name(root, search_name);
 }
--- Telephone_Directory_Main.cpp
+++ Telephone_Directory_Main.cpp
@@ -10,110 +10,121 @@ int main()
 {
 	Phone_Directory directory;
 	int ch;
-	
+
 	do
 	{
 		cout << "\n1. Insert a new record\n";
 		cout << "2. Delete an existing record\n";
 		cout << "3. Display the phone directory\n";
 		cout << "4. Search by phone number\n";
-		cout << "5. Partial matches\n";
-		cout << "6. Frequently searched phone number\n";
-		cout << "7. Search By Name\n";
-        cout << "8. Exit\n\n";
+		cout << "5. Search By Name\n";
+		cout << "6. Partial matches\n";
+		cout << "7. Frequently searched phone number\n";
+		cout << "8. Exit\n\n";
 		cout << "Enter your choice : ";
 		cin >> ch;
-		switch(ch)
+		switch (ch)
 		{
-			case 1:
-				{
-					string name, phone_num;
-					name = accept_name();
-					phone_num = accept_phone_num();
-					directory.Insert_Record(phone_num, name);
-					break;
-				}
-			
-			case 2:
-				{
-					string phone_num;
-					phone_num = accept_phone_num();
-					directory.Delete_Record(phone_num);
-					cout << "\nThe record has been deleted !\n";
-					break;
-				}
-			
-			case 3:
-				{
-					directory.count = 0;
-					if(directory.Is_Empty())
-					{
-						cout << endl << "\nThe phone directory doesn't have any records\n";
-						break;
-					}
-					directory.Display_Records(directory.root);
-					cout << endl;
-					break;
-				}
-			
-			case 4:
-				{
-					string phone_num;
-					phone_num = accept_phone_num();
-					directory.search_key(phone_num);
-					break;
-				}
-			
-			case 5:
-				{
-					string partial_detail;
-					int f=0;
-					cout << endl << "Enter the phone number/name : ";
-					getline(cin >> ws, partial_detail);
-					for(int i=0;i<partial_detail.length();i++){
-						if (( partial_detail[i]>= 65 && partial_detail[i] <= 90)
-        						|| (partial_detail[i] >= 97 && partial_detail[i] <= 122)){
-									f=1;
-									break;
-								}
-					}
-					if(f==0)	directory.Partial_Matches(partial_detail);
-					if(f==1)	directory.Partial_Matches_Name(partial_detail);
-					break;
-				}	
-			
-			case 6:
-				directory.Frequent_keys();
-				break;
-		
-            case 7:
-                string search_name;
-                search_name = accept_name();
-                
-                if(!directory.name_found(search_name))
-                cout << "\nThere are no records with name " << search_name << endl;
-                
-                else
-                {
-                    cout << "\n--------------------------------------------\n";
-	                cout << "Name\t\t\tPhone number" << endl;
-	                cout << "--------------------------------------------\n";
-	                directory.search_by_name(search_name);
-                    
-                    cout << "--------------------------------------------\n";
-                }
+		case 1:
+		{
+			string name, phone_num;
+			name = accept_name();
+			phone_num = accept_phone_num();
+			directory.Insert_Record(phone_num, name);
+			break;
+		}
 
+		case 2:
+		{
+			string phone_num;
+			phone_num = accept_phone_num();
+			directory.Delete_Record(phone_num);
+			cout << "\nThe record has been deleted !\n";
+			break;
+		}
+
+		case 3:
+		{
+			directory.count = 0;
+			if (directory.Is_Empty())
+			{
+				cout << endl
+					 << "\nThe phone directory doesn't have any records\n";
 				break;
-        }
-		
-	} while(ch != 8);
+			}
+			directory.Display_Records(directory.root);
+			cout << endl;
+			break;
+		}
+
+		case 4:
+		{
+			string phone_num;
+			phone_num = accept_phone_num();
+			directory.search_key(phone_num);
+			break;
+		}
+
+		case 5:
+		{
+			string search_name;
+			search_name = accept_name();
+
+			if (!directory.name_found(search_name))
+				cout << "\nThere are no records with name " << search_name << endl;
+
+			else
+			{
+				cout << "\n--------------------------------------------\n";
+				cout << "Name\t\t\tPhone number" << endl;
+				cout << "--------------------------------------------\n";
+				directory.search_by_name(search_name);
+
+				cout << "--------------------------------------------\n";
+			}
+			break;
+		}
+
+		case 6:
+		{
+			string partial_detail;
+			int f = 0;
+			cout << endl
+				 << "Enter the phone number/name : ";
+			getline(cin >> ws, partial_detail);
+			for (int i = 0; i < partial_detail.length(); i++)
+			{
+				if ((partial_detail[i] >= 65 && partial_detail[i] <= 90) || (partial_detail[i] >= 97 && partial_detail[i] <= 122))
+				{
+					f = 1;
+					break;
+				}
+			}
+			if (f == 0)
+				directory.Partial_Matches(partial_detail);
+			if (f == 1)
+				directory.Partial_Matches_Name(partial_detail);
+			break;
+		}
+
+		case 7:
+		{
+			directory.Frequent_keys();
+			break;
+		}
+		default:
+			cout<<"\nEnter the correct choice!!!\n";
+		}
+
+	} while (ch != 8);
 	return 0;
 }
 
 string accept_phone_num()
 {
 	string phone_num;
-	cout << endl << "Enter the phone number : ";
+	cout << endl
+		 << "Enter the phone number : ";
 	getline(cin >> ws, phone_num);
 	return phone_num;
 }
@@ -121,7 +132,8 @@ string accept_phone_num()
 string accept_name()
 {
 	string name;
-	cout << endl << "Enter the person's name : ";
+	cout << endl
+		 << "Enter the person's name : ";
 	getline(cin >> ws, name);
 	return name;
 }
